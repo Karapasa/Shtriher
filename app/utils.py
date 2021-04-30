bb9364fe53ff85bf193348fe33b956ca61b19940
Karapasa <alexmixpetrov@gmail.com>
2021-04-30 17:34:42 +0300

    add logger

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,14 @@
 import os
 import math
 import barcode
+import logging.config
+
+from settings import logging_config
 from PIL import Image, ImageDraw, ImageFont
 from barcode.writer import ImageWriter
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger('shtriher_logger')
 
 dir_name = os.path.join(os.getcwd(), 'app/static/labels/')
 
@@ -26,6 +32,9 @@ class Etich():
         self.field = field
 
     def _get_font_size(self, data):
+        if len(''.join(data)) > 700:
+            logger.debug('Превышен лимит символов для %s - %s', self.compy, self.number)
+            return Exception
         size_table = {18: (7, 37), 16: (8, 40), 14: (9, 51), 12: (10, 59), 10: (11, 69)}
         for size in size_table:
             quan_total_str = 0
@@ -57,39 +66,41 @@ class Etich():
     def _get_font(self, size):
         return ImageFont.truetype("DejaVuSansMono.ttf", size, encoding='utf-8')
 
-    def get_company(self):
+    def _get_company(self):
         return 'Название компании: ' + self.compy if self.compy else ''
 
-    def get_sku(self):
+    def _get_sku(self):
         return '\nАртикул: ' + self.sku if self.sku else ''
 
-    def get_name(self):
+    def _get_name(self):
         return '\nНаименование продукции: ' + self.name if self.name else ''
 
-    def get_field(self):
+    def _get_field(self):
         return '\nОписание: ' + self.field if self.field else ''
 
-    def get_shtrich(self):
+    def _get_shtrich(self):
         a1 = Code(self.number)
         a1.save_code()
 
-    def create_etich(self):
+    def _create_etich(self):
         img = Image.new('RGBA', (455, 284), 'white')
         idraw = ImageDraw.Draw(img)
-        data = tuple(x for x in (self.get_company(), self.get_name(), self.get_sku(), self.get_field()) if x)
+        data = tuple(x for x in (self._get_company(), self._get_name(), self._get_sku(), self._get_field()) if x)
         size, len_str = self._get_font_size(data)
+        logger.debug('Размеры шрифта (%s, %s); Кол-во символов %s', size, len_str, len(''.join(data)))
         txt = ''.join((self._get_text_per_str(x, len_str) if len(x) >= len_str else x for x in data))
         print(txt)
         x, y = 25, 10
         idraw.multiline_text((x,y), text=txt, fill='black', font=self._get_font(size), spacing=2, align='left')
-        self.get_shtrich()
+        self._get_shtrich()
         sht = Image.open(dir_name + self.number + '_sht.png', 'r').resize((250, 120))
 
         img.paste(sht, (102, 170))
         return img
 
     def save_etich(self):
-        img = self.create_etich()
+        img = self._create_etich()
+        logger.debug('Создана этикетка с %s для %s', self.number, self.compy)
         img.save(dir_name + self.number + '_et.png')
 
 class Code():
@@ -103,11 +114,12 @@ class Code():
         self.name = dir_name + self.number + "_sht"
 
 
-    def generate(self):
+    def _generate(self):
         return barcode.get(Code.CODE, self.number, writer=ImageWriter())
 
     def save_code(self):
-        ean = self.generate()
+        ean = self._generate()
+        logger.debug('Создан штрихкод %s', self.number)
         ean.save(self.name,
                  options={'background': 'white', 'module_width': 0.4, 'module_height': 20, 'foreground': 'black',
                           'text_distance': 1, 'font_size': 11})
